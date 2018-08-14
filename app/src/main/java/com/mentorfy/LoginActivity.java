99584f149af0646a2e1ae305d6305fcc50e7037c
Eweb <testdemo198@gmail.com>
2018-08-14 15:09:53 +0530

    first upload

---
+++ app/src/main/java/com/mentorfy/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.mentorfy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.GONE);
+        setSupportActionBar(toolbar);
+    }
+
+}
