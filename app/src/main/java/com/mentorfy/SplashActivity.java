36c87832f6229c89c08b32f0bd19c37edb0add87
Eweb <testdemo198@gmail.com>
2018-08-14 13:27:51 +0530

    first upload

--- app/src/main/java/com/mentorfy/SplashActivity.java
+++ app/src/main/java/com/mentorfy/SplashActivity.java
@@ -1,21 +1,24 @@
 package com.mentorfy;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 3000;
     Toolbar toolbar;
-
+    private TextView tvLogo,tvTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        initUI();
         toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.GONE);
         setSupportActionBar(toolbar);
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -23,5 +26,15 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         },SPLASH_DISPLAY_LENGTH);
+    }
+
+    private void initUI() {
+        tvLogo=findViewById(R.id.tvLogo);
+        tvTitle=findViewById(R.id.tvTitle);
+        Typeface title = Typeface.createFromAsset(getAssets(),  "simplebold.otf");
+        Typeface logo = Typeface.createFromAsset(getAssets(),  "simplebolditalic.otf");
+        tvLogo.setTypeface(logo);
+        tvTitle.setTypeface(title);
+
     }
 }
