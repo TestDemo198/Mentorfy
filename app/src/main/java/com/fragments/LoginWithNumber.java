556a5f967629cd6904cc469a44eb81e87cd7d12c
Eweb <testdemo198@gmail.com>
2018-08-14 17:01:39 +0530

    first upload

---
+++ app/src/main/java/com/fragments/LoginWithNumber.java
@@ -0,0 +1,20 @@
+package com.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mentorfy.R;
+
+public class LoginWithNumber extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_login_number,container,false);
+        return view;
+    }
+}
