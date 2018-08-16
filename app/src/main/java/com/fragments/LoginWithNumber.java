a51e96177ede1d51fe0618e20c53d9ae9b86f851
Eweb <testdemo198@gmail.com>
2018-08-16 11:16:55 +0530

    first upload

--- app/src/main/java/com/fragments/LoginWithNumber.java
+++ app/src/main/java/com/fragments/LoginWithNumber.java
@@ -7,14 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.mentorfy.R;
 
-public class LoginWithNumber extends Fragment {
+public class LoginWithNumber extends Fragment implements View.OnClickListener {
+    Button bContinue;
+    Fragment fragment;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_login_number,container,false);
+        View view = inflater.inflate(R.layout.fragment_login_number, container, false);
+        bContinue = view.findViewById(R.id.bContinue);
+        bContinue.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        fragment = new ProtectPasswordFragments();
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).addToBackStack("").commit();
     }
 }
