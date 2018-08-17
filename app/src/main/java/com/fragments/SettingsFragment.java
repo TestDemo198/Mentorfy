61ff5f75b2673caaf13a8af4c730b702fd0b56c1
Eweb <testdemo198@gmail.com>
2018-08-17 19:09:03 +0530

    first upload

--- app/src/main/java/com/fragments/SettingsFragment.java
+++ app/src/main/java/com/fragments/SettingsFragment.java
@@ -7,14 +7,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.mentorfy.R;
 
-public class SettingsFragment extends Fragment{
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+   LinearLayout llNotificationSetting,llContactFAQ;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.settings_frag,container,false);
+        llContactFAQ=view.findViewById(R.id.llContactFAQ);
+        llNotificationSetting=view.findViewById(R.id.llNotificationSetting);
+        llNotificationSetting.setOnClickListener(this);
+        llContactFAQ.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.llContactFAQ:
+                break;
+            case R.id.llNotificationSetting:
+                break;
+        }
     }
 }
