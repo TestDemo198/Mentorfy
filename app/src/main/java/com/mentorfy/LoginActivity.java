b84ff9f337d58fcec3ac06939d437c8182c08c1b
Eweb <testdemo198@gmail.com>
2018-08-22 16:13:24 +0530

    first upload

--- app/src/main/java/com/mentorfy/LoginActivity.java
+++ app/src/main/java/com/mentorfy/LoginActivity.java
@@ -2,6 +2,7 @@ package com.mentorfy;
 
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -16,12 +17,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private TextView tvLoginLinkedin, tvLoginPhoneNumber;
     Intent intent;
     Fragment fragment;
+    private TextView tvLogo,tvTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvLogo=findViewById(R.id.tvLogo);
+        tvTitle=findViewById(R.id.tvTitle);
         toolbar.setVisibility(View.GONE);
         setSupportActionBar(toolbar);
         initUI();
@@ -30,9 +34,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void initUI() {
         tvLoginLinkedin = findViewById(R.id.tvLoginLinkedin);
         tvLoginPhoneNumber = findViewById(R.id.tvLoginPhoneNumber);
-
+        Typeface title = Typeface.createFromAsset(getAssets(),  "simplebold.otf");
+        Typeface logo = Typeface.createFromAsset(getAssets(),  "simplebolditalic.otf");
         tvLoginLinkedin.setOnClickListener(this);
         tvLoginPhoneNumber.setOnClickListener(this);
+        tvLogo.setTypeface(logo);
+        tvTitle.setTypeface(title);
     }
 
     @Override
